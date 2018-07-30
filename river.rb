2704d669da5086dea745906ebbfab2b453e6669c
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-30 14:25:53 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -6,13 +6,15 @@ def initialize(name)
 @name = name
 
 end 
+
 def flood
-@discharge = @discharge *1.3
+@discharge = @discharge * 1.3
 
 def dry_up 
   @discharge = @discharge / 2
   
 end
+
 end
 Mississipi = River.new("Mississipi")  
 Hudson = River.new("Hudson") 
